4e8b5a75a9090ffe1b83f3698859fc1b523b446f
Lucas Robertson <lucasrobertson0@gmail.com>
2019-04-29 13:15:22 -0700

    Added modules

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,19 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import {Button} from 'reactstrap';
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-        afsd
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <span className="btn btn-danger btn-pill">Test</span>
+                <div className="card" style={{margin: '5rem'}}>
+                    <div className="card-body">
+                        test
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
